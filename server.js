af480493cb298b057008f729ef9336dc43868ae6
Sam Thompson <electronic62794@yahoo.com>
2019-04-18 22:43:50 -0700

    routes set up

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/homework", { useNewUrlParser: true });
 
 
 app.get("/scrape", function (req, res) {
@@ -54,9 +54,47 @@ app.get("/scrape", function (req, res) {
                     .catch(function (err) {
                         console.log(err);
                     })
-        })
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+
+app.get("/articles", function(req, res){
+    db.Article.find({})
+    .then(function(dbArticle){
+        res.json(dbArticle);
     })
-})
+    .catch(function(err){
+        res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res){
+    db.Article.findOne({_id: req.params.id})
+
+    .populate("note")
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res){
+    db.Note.create(req.body)
+    .then(function(dbNote){
+
+        return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+    })
+    .then(function(dbArticle){
+        res.json(dbArticle)
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+});
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT);
